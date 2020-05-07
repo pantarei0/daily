42911c5833dc8ed6e5f617f79a604b1ab0ac6ce2
pantarei0 <pantarei0@yahoo.co.kr>
2020-05-08 08:35:56 +0900

    Update 20200507.sh

--- 2020/202005/20200507.sh
+++ 2020/202005/20200507.sh
@@ -1,11 +1,20 @@
+#로그 파일이 너무 크다. 파일 분할, ybd, 
+
 cp was.20200507.log ~/log.0507
+
 split -b 10m was.20200507.log
+
 ls
-#xaa    xab
-#...
-#xax
+#was.20200507.log  xai
+#xaa               xaj
+#xab               xak
+...
+#xah
 
 grep 11:14:26 x*
-#xav  . .. ..
-#xav  . .. ..
-#
+#xav:2020-05-07 11:14:26 ...
+#xav:2020-05-07 11:14:26 ...
+#...
+
+vi xav
+/11:14:26
