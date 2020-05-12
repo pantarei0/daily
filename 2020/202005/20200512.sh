3b6001c866e503a6f4f8a9fda18f984daf5de20c
pantarei0 <pantarei0@yahoo.co.kr>
2020-05-12 21:48:18 +0900

    Update 20200512.sh

--- 2020/202005/20200512.sh
+++ 2020/202005/20200512.sh
@@ -1,8 +1,7 @@
 #계산기가 필요한데, 
 
-이제는 별짓을 다한다. 폿팅
-
-dsu87dev#/home/jeus_hec> cat n.dat | awk '{sum+=$1; print $0} END {print "sum =",sum}'
+이제는 별짓을 다한다. 폿팅. 이거 파일 다시 만들기.또는 이럭삭제
+ cat n.dat | awk '{sum+=$1; print $0} END {print "sum =",sum}'
 1
 2
 3
@@ -11,7 +10,7 @@ dsu87dev#/home/jeus_hec> cat n.dat | awk '{sum+=$1; print $0} END {print "sum ="
 
 sum = 15
 
-dsu87dev#/home/jeus_hec> export sum=0
+export sum=0
 dsu87dev#/home/jeus_hec> for n in `cat n.dat`;^Jdo^Jsum=`expr $sum + $n`^Jdone
 dsu87dev#/home/jeus_hec> echo $sum
 15
