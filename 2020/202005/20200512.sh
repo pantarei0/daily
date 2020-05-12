5e491c4ceb4b0052e2a642e848f3b8aaf3273249
pantarei0 <pantarei0@yahoo.co.kr>
2020-05-12 21:47:30 +0900

    Update 20200512.sh

--- 2020/202005/20200512.sh
+++ 2020/202005/20200512.sh
@@ -2,3 +2,16 @@
 
 이제는 별짓을 다한다. 폿팅
 
+dsu87dev#/home/jeus_hec> cat n.dat | awk '{sum+=$1; print $0} END {print "sum =",sum}'
+1
+2
+3
+4
+5
+
+sum = 15
+
+dsu87dev#/home/jeus_hec> export sum=0
+dsu87dev#/home/jeus_hec> for n in `cat n.dat`;^Jdo^Jsum=`expr $sum + $n`^Jdone
+dsu87dev#/home/jeus_hec> echo $sum
+15
